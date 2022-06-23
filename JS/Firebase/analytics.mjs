23321970487458c713bc6d7db17fb6a63cc81411
Riley Griffis <ragriffis@gmail.com>
2022-06-23 17:28:51 -0400

    Fix Firebase to add analytics

---
+++ JS/Firebase/analytics.mjs
@@ -0,0 +1,24 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCoSxR4qkdQhHvvC6YIcRii2vsm8rtu0Vk",
+    authDomain: "snap-booth-photos.firebaseapp.com",
+    databaseURL: "https://snap-booth-photos-default-rtdb.firebaseio.com",
+    projectId: "snap-booth-photos",
+    storageBucket: "snap-booth-photos.appspot.com",
+    messagingSenderId: "350438842610",
+    appId: "1:350438842610:web:dc05af5e1b118c1e7b706b",
+    measurementId: "G-BZW342QWVY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  
